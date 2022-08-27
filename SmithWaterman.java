616b76fe1cd54e5d990e7d149eb322fcc91e4429
Pranta Sarker <prantacse04@gmail.com>
2022-08-28 02:17:50 +0600

    Add files via upload

---
+++ SmithWaterman.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smithwaterman;
+import java.util.*;
+
+/**
+ *
+ * @author FC
+ */
+
+class localAlignment{
+    int [][] alignment;
+    char [] ls, rs;
+    
+    void initialization(int row, int col)
+    {
+        alignment = new int [row][col];
+        
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                alignment[i][j] = 0;
+            }
+        }
+    }
+    
+    void calculateAlignmentScore(int row, int col, int match, int mismatch, int gap)
+    {
+        int horizontal=0, vertical=0, diagonal=0, as = 0;
+        
+        for(int i=1; i<row; i++)
+        {
+            for(int j=1; j<col; j++)
+            {
+                //System.out.print(alignment[i][j] + " ");
+                horizontal = alignment[i][j-1] + gap;
+                if(horizontal < 0) horizontal = 0;
+                vertical = alignment[i-1][j] + gap;
+                if(vertical < 0) vertical = 0;
+                //check diagonal
+                alignment[i][j] = alignment[i-1][j-1];
+                if(ls[i-1] == rs[j-1])
+                {
+                    diagonal = alignment[i][j] + match;
+                    System.out.print(ls[i-1] + " " + rs[j-1] + " " + diagonal + "\n");
+                }
+                
+                else
+                {
+                    diagonal = alignment[i][j] + mismatch;
+                }
+                
+                if(diagonal < 0) diagonal = 0;
+                
+                alignment[i][j] = Math.max(horizontal, vertical);
+                alignment[i][j] = Math.max(alignment[i][j], diagonal);
+                as = Math.max(as, alignment[i][j]);
+                
+                //System.out.println("");
+            } 
+        }
+        
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                System.out.print(alignment[i][j] + " ");
+            }
+            
+            System.out.println("");
+        }
+        
+        System.out.println("Alignment Score is = " + as);
+    }
+}
+
+
+public class SmithWaterman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        localAlignment la = new localAlignment();
+        Scanner sc = new Scanner(System.in);
+        String LS = "CTCGTC";
+        String RS = "AGCGTAG";
+        int m = LS.length()+1, n = RS.length()+1;
+        //int [][] alignment = new int [m][n];
+        int match = 10, mismatch = -5, gap = -7;
+        //initialization
+        la.ls = LS.toCharArray();
+        la.rs = RS.toCharArray();
+        la.initialization(m, n);
+        la.calculateAlignmentScore(m, n, match, mismatch, gap);
+    }
+    
+}
